438d1b582a3f20caae5af2d18e493188770a992d
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-07-04 18:07:22 +0530

    commit

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -78,6 +78,7 @@ public class SubscriptionService {
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
         User user = userRepository.findById(userId).get();
+        if(user==null)return 0;
 
         if(user.getSubscription().equals("ELITE"))
         {
